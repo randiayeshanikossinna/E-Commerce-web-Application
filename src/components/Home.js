2da4cf71c03f8ca3e8a67f93d987069763dab6d2
randiayeshanikossinna <randiayeshani@gmail.com>
2021-06-30 00:56:06 -0700

    Navbar is created

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Navbar } from './Navbar';
 import { Products } from './Products';
+import '../css/Home.css';
 
 export const Home = () => {
     return (
-        <div>
+        <div className="wrapper">
             <Navbar />
             <Products/>
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../images/th-removebg-preview (2).png';
+
+
 
 export const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className="navbox">
+            <div className="leftside">
+                <img src={logo} alt=""/>
+            </div>
+            <div className="rightside">
+                <Link to="/signup" className="navlinks">SIGN UP</Link>
+                <Link to="/login" className="navlinks">LOGIN</Link>
+            </div>
         </div>
     )
 }
